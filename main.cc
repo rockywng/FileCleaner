002a1d2bf84d0019e1d0949e802025c286e2eb3d
Rocky Wang <55059833+rockywng@users.noreply.github.com>
2022-04-10 20:03:37 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -12,15 +12,19 @@ using namespace std;
 int main(int argc, char *argv[]) {
     // Require full path to file
     string name = argv[1];
-    //string name = "jojo.h";
+    // Setup file stream
     ofstream datafile;
-    // Add absolute path
+    // Add absolute path to data file
     datafile.open("data.dat", ios_base::app);
+    // Assign tuple to clean values
     tuple<int, int, int, int, int> values = clean(name);
+    // Retrieve cleaning values
     int spaces = get<0>(values);
     int newlines = get<1>(values);
     int ifndefs = get<2>(values);
     int defines = get<3>(values);
+    // Write cleaning values to datafile
     datafile << name << ", " << spaces << ", " << newlines << ", " << ifndefs << ", " << defines << endl;
+    // Inform user of succesful cleaning
     cout << "The file has been successfully cleaned." << endl;
 }
