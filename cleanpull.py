14edafaf832fcea32b5af4790d38645e93a9ad87
Rocky Wang <55059833+rockywng@users.noreply.github.com>
2022-04-10 20:04:43 -0400

    Update cleanpull.py

--- cleanpull.py
+++ cleanpull.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
         sys.exit()
     # Get path to file from command line args
     path = str(sys.argv[1])
+    # Connect to MongoDB server running on default port
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["cleanerdata"]
     mycol = mydb["cleaned"]
@@ -22,14 +23,16 @@ if __name__ == "__main__":
     # Get all entries based on provided path
     y = mycol.find({"path": path}).limit(1).sort([('$natural',-1)])
     entries = list(y)
-    #print("found")
+    # Determine if file entries can be found
     if (len(entries) == 0):
         print("Data on this file could not be found.")
     else:
+        # Retrieve data on previous cleaning
         spaces = entries[0]['spaces']
         newlines = entries[0]['newlines']
         ifndefs = entries[0]['ifndefs']
         defines = entries[0]['defines']
+        # Inform user of previous cleaning metrics
         print("Here are the changes that were made the last time you cleaned this file.")
         print("Spaces Added: " + str(spaces))
         print("Newlines Added: " + str(newlines))
