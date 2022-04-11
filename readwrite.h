1d3b04d739edb135f3956049e98e0d22998e70d6
Rocky Wang <55059833+rockywng@users.noreply.github.com>
2022-04-10 20:06:20 -0400

    Update readwrite.h

--- readwrite.h
+++ readwrite.h
@@ -7,8 +7,10 @@
 #include <vector>
 #include <iterator>
 
+// Write strings from provided vector of strings to provided file
 void write(std::string filename, std::vector<std::string> lines);
 
+// Read lines from given file and return them as a vector of strings
 std::vector<std::string> readFile(std::string filename);
 
 #endif
